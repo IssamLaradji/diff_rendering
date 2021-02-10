73e73183233a3f2a7ae00397bdffeaef44474069
Toolkit User <issam.laradji@gmail.com>
2021-02-10 01:14:21 +0000

    added other optims

--- exp_configs.py
+++ exp_configs.py
@@ -1,15 +1,21 @@
 from haven import haven_utils as hu
 
 EXP_GROUPS = {}
+EXP_GROUPS['project1'] = pass
+EXP_GROUPS['project2'] = pass
+EXP_GROUPS['project3'] = pass
+EXP_GROUPS['project4'] = pass
+EXP_GROUPS['project5'] = pass
+
 EXP_GROUPS['moved_block'] =  [
-    {
-            "source_scene": 
-                          {'name':'moved_block'},
-            "target_scene": 
-                          {'name':'basic'},
-            "model": 
-                          {'name':'basic_renderer', 'opt':'sps'},
-         },
+    # {
+    #         "source_scene": 
+    #                       {'name':'moved_block'},
+    #         "target_scene": 
+    #                       {'name':'basic'},
+    #         "model": 
+    #                       {'name':'basic_renderer', 'opt':'sps'},
+    #      },
          {
             "source_scene": 
                           {'name':'moved_block'},
